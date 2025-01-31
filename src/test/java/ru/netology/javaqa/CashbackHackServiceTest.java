667d883ea0b3128595e0d0c99af8bcc870648c9e
Gorenko Maria <kirillovsub@yandex.ru>
2025-01-31 11:57:58 +0300

    Changes in CashbackHackServiceTest.java

--- src/test/java/ru/netology/javaqa/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/javaqa/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.javaqa;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 public class CashbackHackServiceTest {
