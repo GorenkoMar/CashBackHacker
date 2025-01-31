35482aa9f03ae232cad475349a45f7b8834f8d45
Gorenko Maria <kirillovsub@yandex.ru>
2025-01-31 10:49:32 +0300

    Add changes in class CashbackHackServiceTest.java

--- src/test/java/ru/netology/javaqa/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/javaqa/CashbackHackServiceTest.java
@@ -4,9 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-import static org.testng.Assert.assertEquals;
-
-
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
@@ -14,25 +11,30 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldAmount1() {
 
-        assertEquals(service.remain(1), 999);
+        Assert.assertEquals(service.remain(1), 999);
     }
 
     @Test
     public void shouldAmountZero() {
 
-        assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(service.remain(0), 1000);
     }
 
     @Test
     public void shouldMAmount999() {
 
-        assertEquals(service.remain(999), 1);
+        Assert.assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void shouldMAmount1000() {
+        Assert.assertEquals(service.remain(1000), 0);
     }
 
     @Test
     public void shouldAmount500() {
 
-        assertEquals(service.remain(500), 500);
+        Assert.assertEquals(service.remain(500), 500);
     }
 
 
